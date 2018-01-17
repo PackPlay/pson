bd7b9654929bede163c326a7bb29f97e0613e40c
Poon Wu <poon.wuthi@gmail.com>
2018-01-17 16:53:48 +0700

    Fixed parsers

--- index.js
+++ index.js
@@ -1,30 +1,80 @@
 const _ = require('lodash');
 
-const findById = (id, obj) => {
-    for(let i=0; i < obj['entities'].length; i++) {
-        let e = obj['entities'][i];
-        if(e.id === id) {
-            return e;
+
+// find entity by id
+function findById(id, arr){
+    if(id) {
+        for(let i=0; i < arr.length; i++) {
+            let e = arr[i];
+            if(e.id === id) {
+                return e;
+            }
         }
     }
     throw new Error('cannot find id', id);
 };
-const getArrayWithEntity = (arr, obj) => {
-    let newArray = [];
-    for(let i=0; i<arr.length; i++) {
-        newArray.push(findById(arr[i], obj));
-    }
-
-    return newArray;
+function createFindById(arr) {
+    return function(id) {
+        return findById(id, arr);
+    };
 };
 
-const traverse = (obj, cb) => {
+// turn entity into id
+function getId(entity) {
+    return entity.id;
+}
+
+// traverse tree
+function traverse(obj, cb) {
     cb(obj);
     for(let i =0; i < obj.children.length; i++) {
         let r = obj.children[i];
         traverse(r);
     }
 }
+
+/**
+ * Handle each entity differently
+ * it will callback to fn for each sub-entities--- used for serial/deserial
+ * @param {*Object} obj any Entity 
+ * @param {*Function} fn callback for each entity to be serial/deserial 
+ */
+function handleEntity(obj, fn) {
+    let className = obj.className;
+    // let key = [];
+
+    if(className === 'Line') {
+        obj.a = fn(obj.a);
+        obj.b = fn(obj.b);
+    }
+    if(className === 'Arc') {
+        obj.a = fn(obj.a);
+        obj.b = fn(obj.b);
+        obj.center = fn(obj.center);
+    }
+    if(className === 'Polyline') {
+        obj.segments = obj.segments.map(e => fn(e));
+    }
+    if(className === 'Graph') {
+        traverse(obj, o => o.data = fn(o.data));
+    }
+    if(className === 'Panel') {
+        obj.outer = obj.outer.map(e => fn(e));
+        obj.inner = obj.inner.map(e => fn(e));
+    }
+    return obj;
+}
+
+// Turn ids into entities
+function deferenceEntity(obj, entities) {
+    let find = createFindById(entities);
+    return handleEntity(obj, find);
+}
+// Turn entities into ids
+function referenceEntity(obj) {
+    return handleEntity(obj, getId);
+}
+
 const pson = {
     read(obj) {
         if(_.isString(obj)) {
@@ -32,25 +82,16 @@ const pson = {
         }
         
         let newObj = {};
-        
-        // link graph to entities
-        _.forEach(obj['entities'], (k, v) => {
-            if(v.className === 'Graph') {
-                traverse(v, o => {
-                    o.data = findById(o.data);
-                });
-            }
-        });
-        
+        let find = createFindById(obj['entities']);
+
         // link pson array to entities
         _.forOwn(obj, (k, v) => {
-            if(k !== 'metadata' || k !== 'entities') {
-                newObj[k] = getArrayWithEntity(v, obj);
+            if(_.isArray(v)) {
+                newObj[k] = v.map(e => deference(e, obj['entities']));
             } else {
                 newObj[k] = v;
             }
         });
-
         return newObj;
     }
 };
